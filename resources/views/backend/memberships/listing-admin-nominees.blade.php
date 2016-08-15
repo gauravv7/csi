b2789d4eb1de8ea9ed8033acdde2a11381977845
himanshu-uppal <himanshu94uppal@gmail.com>
2016-08-15 16:11:23 +0530

    profile shown in nominee listing

--- resources/views/backend/memberships/listing-admin-nominees.blade.php
+++ resources/views/backend/memberships/listing-admin-nominees.blade.php
@@ -271,14 +271,14 @@
         });
 
         $('#profile').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget); // Button that triggered the modal
-            var id = button.data('id');
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            url = window.location.origin + "/userprofile/" + id + "/view";
-            var modal = $(this);
-            modal.find('.modal-body iframe').attr('src', url);
-        });
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var id = button.data('id');
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    url = window.location.origin+"/admin/memberships/profile/"+id+"/view";
+    var modal = $(this);
+    modal.find('.modal-body iframe').attr('src', url);
+  }); 
     </script>
     <script src={{ asset("js/validateit.js") }}></script>
 @endsection
